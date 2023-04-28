483cffae1c522f4b2c93d6070c48ad7894b32721
Aleksandr <aleksandr.v.dikarev@gmail.com>
2023-04-28 15:52:20 +0300

    Add files via upload

---
+++ uBear/UI/InfoViewDialog.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace uBear.UI
+{
+    partial class InfoViewDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.okBtn = new System.Windows.Forms.Button();
+            this.txb = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // okBtn
+            // 
+            this.okBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okBtn.Location = new System.Drawing.Point(468, 246);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(89, 30);
+            this.okBtn.TabIndex = 4;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            // 
+            // txb
+            // 
+            this.txb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txb.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txb.Location = new System.Drawing.Point(12, 12);
+            this.txb.Name = "txb";
+            this.txb.ReadOnly = true;
+            this.txb.Size = new System.Drawing.Size(545, 208);
+            this.txb.TabIndex = 5;
+            this.txb.Text = "";
+            // 
+            // InfoViewDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 288);
+            this.Controls.Add(this.txb);
+            this.Controls.Add(this.okBtn);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "InfoViewDialog";
+            this.ShowIcon = false;
+            this.Text = "Info";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.RichTextBox txb;
+    }
+}
---
+++ uBear/UI/InfoViewDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uBear.UI
+{
+    public partial class InfoViewDialog : Form
+    {
+        #region Properties
+
+        public string InfoText
+        {
+            get => txb.Text;
+            set => txb.Text = value;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public InfoViewDialog()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
